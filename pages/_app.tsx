b7d68710c1aaad997677462c0c762fb312fbf956
musa gillani <musagillaniwork@gmail.com>
2023-06-19 18:44:42 +0500

    removed unused index page moved payments page to index page added next-themes package added button to toggle theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import '@/styles/globals.css'
+import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+      <div className='flex justify-end w-full p-5'>
+        <ModeToggle />
+      </div>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
